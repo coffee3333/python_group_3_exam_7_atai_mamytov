ae39fed2060d4ca6fe9542e6043ca9b27ef87d4d
Atay <atay.mamytov@gmail.com>
2019-10-19 07:00:00 +0000

    Created model and information for DB, made migrations and dump data

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Poll(models.Model):
+    question = models.TextField(max_length=3000, null=False, blank=True, verbose_name='Question')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Created time')
+
+    def __str__(self):
+        return self.question
+
+
+class Choice(models.Model):
+    poll = models.ForeignKey('webapp.Poll', related_name='choice', on_delete=models.CASCADE, verbose_name='poll')
+    answer_option = models.TextField(max_length=3000, null=False, blank=True, verbose_name='Answer option')
+
+    def __str__(self):
+        return self.answer_option
+
